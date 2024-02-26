4a4dc673bfab7eef699f3ccb323212b3a63919b9
Rushi_0508 <rushigandhi14@gmail.com>
2024-02-26 17:06:35 +0530

    Created Brand View Page

---
+++ src/app/brand/[id]/components/Collaborations.tsx
@@ -0,0 +1,49 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+import Link from 'next/link'
+
+function Collaborations({ user }: any) {
+    return (
+        <>
+            <div>
+                <p className='text-xl flex items-baseline gap-2 font-semibold'>
+                    Collaborations
+                </p>
+                {user?.collaborations && user?.collaborations.length > 0 ?
+                    <>
+                        <Carousel className='mt-3 whitespace-nowrap mx-4'>
+                            {user.collaborations.map((c: any, index: any) => (
+                                <CarouselContent>
+                                    <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/2">
+                                        <Card>
+                                            <CardHeader className='flex gap-4 flex-row'>
+                                                <Avatar className="w-16 h-16">
+                                                    <AvatarImage src={c.creator.avatar} className='object-cover overflow-visible' />
+                                                    <AvatarFallback>CN</AvatarFallback>
+                                                </Avatar>
+                                                <div>
+                                                    <CardTitle>
+                                                        <Link className='hover:underline' href={`/campaign/${c.campaign.id}`}>{c.campaign.name.length > 30 ? c.campaign.name.substring(0, 30) + '...' : c.campaign.name}</Link>
+                                                    </CardTitle>
+                                                    <CardDescription>by <Link className='hover:underline' href={`/creator/${c.creator.id}`}>{c.creator.fullName}</Link></CardDescription>
+                                                </div>
+                                            </CardHeader>
+                                        </Card>
+                                    </CarouselItem>
+                                </CarouselContent>
+                            ))}
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </Carousel>
+                    </>
+                    :
+                    <p className='text-gray-400'>No Collaborations yet.</p>
+                }
+            </div>
+        </>
+    )
+}
+
+export default Collaborations
---
+++ src/app/brand/[id]/components/Header.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Pencil1Icon, ReloadIcon, SewingPinIcon } from '@radix-ui/react-icons'
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+
+function Header({ user, setUser }: any) {
+    return (
+        <div className='border-b-[1px] border-gray-300 p-5 flex justify-between'>
+            <div className="flex gap-4 items-center">
+                <div className='relative'>
+                    <Avatar className="w-20 h-20">
+                        <AvatarImage src={user?.logo} className='object-cover overflow-visible' />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </div>
+                <div>
+                    <p className="font-semibold text-3xl">{user?.name}</p>
+                    <span className="flex items-center mt-1 text-gray-500"><SewingPinIcon className='w-5 h-5' />{user?.state}, {user?.country}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
