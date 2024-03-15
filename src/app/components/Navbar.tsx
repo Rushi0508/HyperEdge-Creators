94851459757e069f09311ac9ea1b6c20b51e80e3
Rushi_0508 <rushigandhi14@gmail.com>
2024-03-15 15:33:11 +0530

    Fixes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -23,7 +23,7 @@ function Navbar() {
   const [notiIndicator, setNotiIndicator] = useState(false)
   const router = useRouter()
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-white">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
           <Link className="mr-6 flex items-center space-x-2" href="/">
